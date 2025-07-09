845c129ebec197a458a436ab250bac184e13624a
Mariana Restrepo <restrepomejiamariana@gmail.com>
2025-07-09 11:51:00 -0500

    init: backend NestJS base2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'], // Pon aquí el puerto de tu Next.js (por defecto 3000)
+    credentials: true,
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
